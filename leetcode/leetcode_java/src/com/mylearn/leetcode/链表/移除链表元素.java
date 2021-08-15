38a0b5f7db52e9ab3ec1e4ccdd48e6c7c0d13af8
dingye <dingyelf@163.com>
2021-08-15 22:02:19 +0800

    commit

---
+++ leetcode/leetcode_java/src/com/mylearn/leetcode/链表/移除链表元素.java
@@ -0,0 +1,29 @@
+package com.mylearn.leetcode.链表;
+
+import org.junit.Test;
+
+public class 移除链表元素 {
+
+    @Test
+    public void test1(){
+
+    }
+
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode pre = dummy;
+        ListNode cur = head;
+        while (cur != null) {
+            if (cur.val != val) {
+                pre = cur;
+                cur = cur.next;
+            } else {
+                pre.next = cur.next;
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+
+}
